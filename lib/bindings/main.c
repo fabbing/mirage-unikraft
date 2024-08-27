47a135c7ea0ed099c01845d79bacc0907c1d5b23
fabbing <fabrice@tarides.com>
2024-08-27 13:40:36 +0200

    Start OCaml runtime

--- lib/bindings/main.c
+++ lib/bindings/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <caml/callback.h>
 
 int main(int argc, char *argv[])
 {
   printf("Hello world from libmirage-unikraft_bindings.a\n");
+  caml_startup(argv);
   return 0;
 }
